1bf6a34f316713353abdd6feaefac31318356e89
meaghanramlakhan8 <mcr8@rice.edu>
2024-01-13 14:27:17 -0600

    update4

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express"); 
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+const port = 3000;
+const server = app.listen(port, () => {
+  const addr = server.address();
+  console.log(`Testing Server listening at http://localhost:${addr.port}`);
+});
+
+module.exports = app;
